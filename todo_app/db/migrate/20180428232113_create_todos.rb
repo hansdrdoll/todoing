aeeb4a49b87f2572ecefc008f20d59f5e0ccecd6
Hans Kirkendoll <hans@hanskirkendoll.com>
2018-04-28 19:36:04 -0400

    reset everything

--- todo_app/db/migrate/20180428232113_create_todos.rb
+++ todo_app/db/migrate/20180428232113_create_todos.rb
@@ -1,10 +1,6 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
-      t.string :key
-      t.boolean :archived
-      t.integer :priority_area
-      t.integer :priority_order
 
       t.timestamps
     end
