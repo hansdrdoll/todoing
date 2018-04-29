907743fc91dba9540ab1758b352222b12767bb10
Hans Kirkendoll <hans@hanskirkendoll.com>
2018-04-29 12:52:28 -0400

    editor works

--- todo_app/db/migrate/20180428232113_create_todos.rb
+++ todo_app/db/migrate/20180428232113_create_todos.rb
@@ -1,6 +1,11 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
+      # TODO: test draft state before making duplicate data
+      t.string :key
+      t.boolean :archived
+      t.integer :priority_area
+      t.integer :priority_order
 
       t.timestamps
     end
