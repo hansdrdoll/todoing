a0f428d17877c17ebc377b8346c9da6be0091c0e
Hans Kirkendoll <hans@hanskirkendoll.com>
2018-04-30 12:50:28 -0400

    immutable data type modification for priority levels applied in gui

---
+++ todo_app/client/src/DraftEditor/PrioritySetters.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { EditorState, Modifier, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import PropTypes from 'prop-types';
+
+class UrgentQuick extends Component {
+  static propTypes = {
+    onChange: PropTypes.func,
+    editorState: PropTypes.object,
+  };
+
+  setUrgentQuick: Function = (): void => {
+    const { editorState, onChange } = this.props;
+    const contentState = Modifier.setBlockData(
+      editorState.getCurrentContent(),
+      editorState.getSelection(),
+      { "urgent": true, "quick": true },
+    );
+    onChange(EditorState.push(editorState, contentState, 'set-priority'));
+  };
+
+  render() {
+    return (
+      <div onClick={this.setUrgentQuick}>⭐1</div>
+    );
+  }
+}
+
+class UrgentSlow extends Component {
+  static propTypes = {
+    onChange: PropTypes.func,
+    editorState: PropTypes.object,
+  };
+
+  setUrgentSlow: Function = (): void => {
+    const { editorState, onChange } = this.props;
+    let contentState = Modifier.setBlockData(
+      editorState.getCurrentContent(),
+      editorState.getSelection(),
+      { "urgent": true, "quick": false },
+    )
+    onChange(EditorState.push(editorState, contentState, 'set-priority'));
+  };
+
+  render() {
+    return (
+      <div onClick={this.setUrgentSlow}>⭐2</div>
+    );
+  }
+}
+
+class NotUrgentSlow extends Component {
+  static propTypes = {
+    onChange: PropTypes.func,
+    editorState: PropTypes.object,
+  };
+
+  setNotUrgentSlow: Function = (): void => {
+    const { editorState, onChange } = this.props;
+    let contentState = Modifier.setBlockData(
+      editorState.getCurrentContent(),
+      editorState.getSelection(),
+      { "urgent": false, "quick": false },
+    )
+    onChange(EditorState.push(editorState, contentState, 'set-priority'));
+  };
+
+  render() {
+    return (
+      <div onClick={this.setNotUrgentSlow}>⭐3</div>
+    );
+  }
+}
+
+class NotUrgentQuick extends Component {
+  static propTypes = {
+    onChange: PropTypes.func,
+    editorState: PropTypes.object,
+  };
+
+  setNotUrgentQuick: Function = (): void => {
+    const { editorState, onChange } = this.props;
+    let contentState = Modifier.setBlockData(
+      editorState.getCurrentContent(),
+      editorState.getSelection(),
+      { "urgent": false, "quick": true },
+    )
+    onChange(EditorState.push(editorState, contentState, 'set-priority'));
+  };
+
+  render() {
+    return (
+      <div onClick={this.setNotUrgentQuick}>⭐4</div>
+    );
+  }
+}
+
+export { UrgentSlow, UrgentQuick, NotUrgentSlow, NotUrgentQuick }
--- todo_app/client/src/DraftEditor/index.js
+++ todo_app/client/src/DraftEditor/index.js
@@ -1,9 +1,28 @@
 
 import React, { Component } from 'react';
-import { EditorState, convertToRaw } from 'draft-js';
+import { EditorState, Modifier, convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
+import PropTypes from 'prop-types';
+import { UrgentSlow, UrgentQuick, NotUrgentSlow, NotUrgentQuick } from './PrioritySetters'
+
+// import { Map } from 'immutable';
 import debounce from 'lodash/debounce';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+
+const styleMap = {
+  'urgent-slow': {
+    color: 'red',
+  },
+  'urgent-quick': {
+    color: 'blue',
+  },
+  'noturgent-slow': {
+    color: 'orange',
+  },
+  'noturgent-quick': {
+    color: 'green',
+  },
+};
 
 class DraftEditor extends Component {
   constructor(props) {
@@ -15,14 +34,15 @@ class DraftEditor extends Component {
 
 onEditorStateChange: Function = (editorState) => {
   this.logStateChange(editorState.getCurrentContent())
-  // this.logStateChange(editorState)
   this.setState({
     editorState,
   });
 };
 
 logStateChange = debounce((editorState) => {
-  console.log(convertToRaw(editorState))
+  const rawState = convertToRaw(editorState)
+  // console.log(rawState)
+  console.table(rawState.blocks)
 }, 2000)
 
 render() {
@@ -33,6 +53,38 @@ render() {
       wrapperClassName="demo-wrapper"
       editorClassName="demo-editor"
       onEditorStateChange={this.onEditorStateChange}
+      toolbarCustomButtons={[<UrgentQuick />, <UrgentSlow />, <NotUrgentSlow />, <NotUrgentQuick />]}
+      customStyleMap={styleMap}
+      toolbar={{
+  options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'colorPicker', 'link', 'emoji', 'history'],
+  inline: {
+    inDropdown: false,
+    className: undefined,
+    component: undefined,
+    dropdownClassName: undefined,
+    options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace'],
+    // bold: { icon: bold, className: undefined },
+    // italic: { icon: italic, className: undefined },
+    // underline: { icon: underline, className: undefined },
+    // strikethrough: { icon: strikethrough, className: undefined },
+    // monospace: { icon: monospace, className: undefined },
+    // superscript: { icon: superscript, className: undefined },
+    // subscript: { icon: subscript, className: undefined },
+  },
+  blockType: {
+    inDropdown: true,
+    options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Blockquote', 'Code', 'Urgent'],
+    // options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Blockquote', 'Code'],
+    className: undefined,
+    component: undefined,
+    dropdownClassName: undefined,
+  },
+
+  list: { inDropdown: true },
+  textAlign: { inDropdown: true },
+  link: { inDropdown: true },
+  // history: { inDropdown: true },
+}}
     />
   )
 }
